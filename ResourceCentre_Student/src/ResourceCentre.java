2186679042b4fd4be72056a1a86a1519357eaf9c
19008826 <19008826@DESKTOP-CS0LFPE>
2020-07-21 16:00:41 +0800

    view

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -149,7 +149,7 @@ public class ResourceCentre {
 		
 		String output = "";
 
-		// write your code here 
+		// write your code here  
 		for (int i = 0; i < chromebookList.size(); i++) {
 
 			output += String.format("%-10s %-30s %-10s %-10s %-20d\n", chromebookList.get(i).getAssetTag(),
